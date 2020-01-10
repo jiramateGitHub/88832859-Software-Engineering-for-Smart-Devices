5e5bf68a038ddbf7857340d89e323523dea5cc58
jiramateGitHub <jiramategithub@gmail.com>
2020-01-10 18:54:40 +0700

    update commit

--- Week5/readme.md
+++ Week5/readme.md
@@ -1,4 +1,4 @@
-Android Fragment& List View Homework 04/01/2563
+Android Fragment & List View Homework 04/01/2563
 - Fragment
 - List View
 - Json Object
---
+++ Week6/app/src/main/java/com/example/week6/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.week6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fragment_RecyclerView = Recycler_view()
+        val manager = supportFragmentManager
+        val transaction = manager.beginTransaction()
+        transaction.replace(R.id.layout, fragment_RecyclerView,"fragment_RecyclerView")
+        transaction.addToBackStack("fragment_list_view")
+        transaction.commit()
+    }
+}
