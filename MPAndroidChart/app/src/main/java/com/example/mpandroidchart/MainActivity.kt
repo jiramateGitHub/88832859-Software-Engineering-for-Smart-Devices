0c9fa1a14c7f8805c510bc251eb7f5a49daffc06
Jiramate Phuaphan <48597732+jiramateGitHub@users.noreply.github.com>
2020-02-28 10:41:52 +0700

    *

---
+++ MPAndroidChart/app/src/main/java/com/example/mpandroidchart/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.mpandroidchart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val MainChart = MainChart()
+        val manager = supportFragmentManager
+        val transaction = manager.beginTransaction()
+        transaction.replace(R.id.contentContainer, MainChart,"fragment_MainChart")
+        transaction.addToBackStack("fragment_MainChart")
+        transaction.commit()
+
+    }
+}
